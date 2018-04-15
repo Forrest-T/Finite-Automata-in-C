9a78745118d4a4a14f18047e4eba85411e94ad19
Forrest Timour <timourf@cs.washington.edu>
2018-04-15 14:37:35 +0200

    added alternate main for debugging

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "string.h"
+#include "stdlib.h"
 #include "stdio.h"
 
 #include "conversion.h"
@@ -6,9 +7,9 @@
 #include "NFA.h"
 #include "DFA.h"
 
-int main(int argc, char **argv) {
+int debug_intermediate(int argc, char **argv) {
     if (argc < 2)
-        die("Usage: ./parser {regexp}\n");
+        die("Usage: ./parser {regexp}");
     printf("\tregex:  /%s/\n", argv[1]);
     int length = strlen(argv[1]);
     printf("\tlength: %d\n", length);
@@ -48,7 +49,63 @@ int main(int argc, char **argv) {
     NFA_destroy(ec);
     NFA_destroy(n);
     DFA_destroy(d);
-    //DFA_destroy(dm);
+    DFA_destroy(dm);
     return 0;
 }
 
+int main(int argc, char **argv) {
+#ifdef DEBUG
+    return debug_intermediate(argc, argv);
+#endif
+    char *alph = NULL;
+    size_t buf_len = 0;
+    if (getline(&alph, &buf_len, stdin) == -1) {
+        free(alph);
+        die("unable to read alphabet");
+    }
+    char *regex = NULL;
+    if (getline(&regex, &buf_len, stdin) == -1) {
+        free(alph);
+        free(regex);
+        die("unable to read regex");
+    }
+    buf_len = 0;
+    NFA *ne = parse(regex, strlen(regex)-1);
+    // ignore the '\n'                  ^^
+    NFA *ec = epsilon_closure(ne);
+    NFA *n = remove_epsilons(ec);
+    DFA *d = NFA_convert(n, alph, strlen(alph)-1);
+    DFA *dm = DFA_minimize(d);
+    DFA_reset(dm);
+    DFA_print(dm);
+    // read lines from stdin and run "dm"
+    char *line = NULL;
+    buf_len = 0;
+    while (getline(&line, &buf_len, stdin) != -1) {
+        int line_len = strlen(line);
+        // if dm accepts the empty string, no need to check
+        int accept = (DFA_reset(dm), DFA_accept(dm));
+        // loop over starting indices to search for substrings
+        for (int i = 0; i < line_len && !accept; i++) {
+            DFA_reset(dm);
+            for (int j = 0; j < line_len-i && !accept; j++) {
+                DFA_read(dm, line[i+j]);
+                if (DFA_accept(dm)) accept = 1;
+            }
+        }
+        // print lines that are matched
+        if (accept) printf("%s", line);
+        free(line);
+        line = NULL;
+    }
+    // clean up
+    free(line);
+    free(alph);
+    free(regex);
+    NFA_destroy(ne);
+    NFA_destroy(ec);
+    NFA_destroy(n);
+    DFA_destroy(d);
+    DFA_destroy(dm);
+    return 0;
+}
